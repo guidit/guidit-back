3faa2b3d2a4bfcdb25c3de0d3642560258d23281
bleetoteelb <sh1993park@gamil.com>
2017-06-08 07:35:24 +0900

    plna/detail

--- routes/plan.js
+++ routes/plan.js
@@ -54,26 +54,32 @@ router.post('/create',function(req,res){
             });
         },function(plan_id,callback){
             var count =0;
-            console.log(daily_plan);
-            console.log(daily_plan[0].sight_list);
-            for(var i=0;i<daily_plan.length;i++){
-                insert_query = 'insert into daily_plan (day_num,plan_id) values ('+Number(daily_plan[i].day_num)+','+plan_id+')';
-                sqlconnection.query(insert_query, function(err,result){
-                    if(err){
-                        console.log(err);
-                        res.jsonp(err);
-                    }
-                    else{
-                        var daily_plan_id = result.insertId;
-                        var sightlist_in_each_plan = daily_plan[count].sight_list;
-                        for(var k=0;k<sightlist_in_each_plan.length;k++) {
-                            insert_query2 = 'insert into daily_to_sight (daily_plan_id,sight_id) values ('+Number(daily_plan_id)+','+sightlist_in_each_plan[k].id+')';
-                            sqlconnection.query(insert_query2);
+            if(daily_plan == null){
+                callback(null,null);
+            }else{
+                for(var i=0;i<daily_plan.length;i++){
+                    insert_query = 'insert into daily_plan (day_num,plan_id) values ('+Number(daily_plan[i].day_num)+','+plan_id+')';
+                    sqlconnection.query(insert_query, function(err,result){
+                        if(err){
+                            console.log(err);
+                            res.jsonp(err);
                         }
-                        count++;
-                        if(count == daily_plan.length){callback(null,null)}
-                    }
-                });
+                        else{
+                            var daily_plan_id = result.insertId;
+                            var sightlist_in_each_plan = daily_plan[count].sight_list;
+                            count++;
+                            if(sightlist_in_each_plan == null){
+                                if(count == daily_plan.length){callback(null,null)}
+                            }else{
+                                for(var k=0;k<sightlist_in_each_plan.length;k++) {
+                                    insert_query2 = 'insert into daily_to_sight (daily_plan_id,sight_id) values ('+Number(daily_plan_id)+','+sightlist_in_each_plan[k].id+')';
+                                    sqlconnection.query(insert_query2);
+                                }
+                                if(count == daily_plan.length){callback(null,null)}
+                            }
+                        }
+                    });
+                }
             }
         }
     ],function(err,result){
@@ -83,7 +89,6 @@ router.post('/create',function(req,res){
         }
     }
     )
-    
 });
 
 router.post('/modify',function(req,res){
@@ -92,8 +97,81 @@ router.post('/modify',function(req,res){
 });
 
 router.get('/detail',function(req,res){
+    var plan_id = req.query['id'];
 
-    // to do;
+    async.waterfall([
+        function(callback){
+            var find_query = 'select * from plan where id='+Number(plan_id);
+            sqlconnection.query(find_query, function(err,result){
+                if(err){
+                    console.log(err);
+                    res.jsonp(err);
+                }else{
+                    callback(null,result[0]);
+                }
+            });
+        },function(plan,callback){
+            var find_query = 'select * from daily_plan where plan_id='+Number(plan.id);
+            sqlconnection.query(find_query,function(err,result){
+                if(err){
+                    console.log(err);
+                    res.jsonp(err);
+                }else{
+                    callback(null,plan,result);
+                }
+            });
+        },function(plan,daily_plan,callback){
+            var count = 0;
+            var sightlist = [];
+            var daily_planlist = [];
+            if(daily_plan.length == 0){
+                respone_json = {"id":plan.id,"name":plan.name,"is_public":plan.is_public,"view_count":plan.view_count};
+                callback(null,respone_json);
+            }
+            else{
+                for(var i=0;i<daily_plan.length;i++){
+                    var find_query = 'select * from daily_to_sight where daily_plan_id='+Number(daily_plan[i].id);
+                    sqlconnection.query(find_query,function(err,result){
+                        if(err){
+                            console.log(err);
+                            res.jsonp(err);
+                        }else{
+                            if(result.length==0){
+                                daily_planlist.push({"id":daily_plan[count].id,"day_num":daily_plan[count].day_num,"review":daily_plan[count].review,"picture":daily_plan[count].picture})
+                                count++;
+                                sightlist = [];
+                                if(count == daily_plan.length){
+                                    respone_json = {"id":plan.id,"name":plan.name,"is_public":plan.is_public,"view_count":plan.view_count,"daily_plan":daily_planlist};
+                                    callback(null,respone_json);
+                                }
+                            }else{
+                                for(var count2=0;count2<result.length;count2++){
+                                    sightlist.push({"id":result[count2].sight_id});
+                                    if(count2 == result.length-1){
+                                        daily_planlist.push({"id":daily_plan[count].id,"day_num":daily_plan[count].day_num,"review":daily_plan[count].review,"picture":daily_plan[count].picture,"sight_list":sightlist})
+                                        count++;
+                                        sightlist = [];
+                                        if(count == daily_plan.length){
+                                            respone_json = {"id":plan.id,"name":plan.name,"is_public":plan.is_public,"view_count":plan.view_count,"daily_plan":daily_planlist};
+                                            callback(null,respone_json);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    ],function(err,result){
+        if(err){ console.log(err); }
+        else {
+            res.jsonp(result);
+        }
+    }
+    )
+    
+
 });
 
 router.get('/list',function(req,res){
